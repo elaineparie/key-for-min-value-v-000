9dce13958dc4ab0841bf9a8fa6f55b6a330a722b
Elaine Anderson <eparie@bu.edu>
2018-05-24 18:46:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
 name_hash.collect do |key, value|
     if value < min_val
-    min_val = value
-    min_key = key
+    min_val = value && min_key = key
     end
   end
   min_key
