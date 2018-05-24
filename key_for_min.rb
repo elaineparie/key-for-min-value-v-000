ec4c0ec655970a28f0e81659eb660df92ce4048c
Elaine Anderson <eparie@bu.edu>
2018-05-24 18:09:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,22 +5,22 @@ def key_for_min_value(name_hash)
   min_val = nil
   min_index = nil
 name_hash.collect do |key, value|
-  if value < min_val 
-    min_val = value 
+  if value < min_val
+    min_val = value
     min_index = key
   end
 end
 
 
-min_val = 2
-[5, 7, 2, 1, 13, 18, 5]
+#min_val = 2
+#[5, 7, 2, 1, 13, 18, 5]
 
-min_val = array[0]
-min_index = 0
-array.each_with_index do |num, index|
-  if num < min_value 
-    min_val = num 
-    min_index = index
-  end
-end
-min_val
+#min_val = array[0]
+#min_index = 0
+#array.each_with_index do |num, index|
+#  if num < min_value
+  #  min_val = num
+  #  min_index = index
+  #end
+#end
+#min_val
